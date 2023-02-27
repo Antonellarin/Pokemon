9b89b4d6cac03a8e1f67efc80ddf0630c0900ab9
Antonellarin <antonellaarin97@gmail.com>
2023-02-27 12:29:15 -0300

    backend terminado

--- PI-Pokemon-main/api/src/routes/index.js
+++ PI-Pokemon-main/api/src/routes/index.js
@@ -1,9 +1,20 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const express = require("express");
+const morgan = require("morgan");
+const pokeRouter = require("./PokemonRouter");
+const typesRouter = require("./TypesRouter");
 
 
 const router = Router();
+//middleware
+router.use(morgan("dev")); //morgan for consola
+router.use(express.json()); //metodo convertidor de json
+
+router.use("/pokemons", pokeRouter);
+router.use("/types", typesRouter);
+
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
